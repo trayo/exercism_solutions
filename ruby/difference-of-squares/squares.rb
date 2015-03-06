bb6519a62ff104eefc4af71d6299c372f27ae473
Travis Yoder <trayoda@gmail.com>
2015-03-06 10:21:31 -0700

    Updated difference of squares to use a proc in the squares methods instead of allowing the user to pass numbers in

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -6,22 +6,26 @@ class Squares
     @sums, @squares = 0, 0
   end
 
-  def square_of_sums(num = users_number)
-    if num < 1
-      sums ** 2
-    else
-      @sums += num
-      square_of_sums(num - 1)
+  def square_of_sums
+    sum = ->(num) do
+      if num < 1
+        sums ** 2
+      else
+        @sums += num and sum[num - 1]
+      end
     end
+    sum[users_number]
   end
 
-  def sum_of_squares(num = users_number)
-    if num < 1
-      squares
-    else
-      @squares += num ** 2
-      sum_of_squares(num - 1)
+  def sum_of_squares
+    square = ->(num) do
+      if num < 1
+        squares
+      else
+        @squares += num ** 2 and square[num - 1]
+      end
     end
+    square[users_number]
   end
 
   def difference
