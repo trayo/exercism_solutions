4cf40194ce7892e20a32d18c1b412accf36d9a1d
Travis Yoder <trayoda@gmail.com>
2015-03-25 09:58:15 -0600

    updated complement and extracted the validation

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,18 +1,23 @@
 module Complement
 
   def self.of_dna(dna_string)
-    if /[^GCTA]/ =~ dna_string
-      raise ArgumentError
-    end
+    valid_strand? /[^GCTA]/, dna_string
 
     dna_string.tr "GCTA", "CGAU"
   end
 
   def self.of_rna(rna_string)
-    if /[^CGAU]/ =~ rna_string
-      raise ArgumentError
-    end
+    valid_strand? /[^CGAU]/, rna_string
 
     rna_string.tr "CGAU", "GCTA"
   end
+
+  private
+
+  def self.valid_strand?(regex, strand)
+    if regex =~ strand
+      raise ArgumentError
+    end
+  end
 end
+
