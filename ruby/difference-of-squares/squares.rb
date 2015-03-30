fbc605e482d62c33c398ae21efe0fb70d8ba4aef
Travis Yoder <trayoda@gmail.com>
2015-03-30 08:47:54 -0600

    changed the variable name to be more generic

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -1,15 +1,15 @@
 class Squares
 
   def initialize(users_number)
-    @range_of_users_number = (1..users_number)
+    @numbers = (1..users_number)
   end
 
   def square_of_sums
-    @squares ||= (@range_of_users_number.reduce(:+)) ** 2
+    @squares ||= (@numbers.reduce(:+)) ** 2
   end
 
   def sum_of_squares
-    @sums ||= @range_of_users_number.reduce { |result, num| result += num ** 2 }
+    @sums ||= @numbers.reduce { |result, num| result += num ** 2 }
   end
 
   def difference
