6fa3c8250017dfc0b1b032f405412eac9780e635
Travis Yoder <trayoda@gmail.com>
2015-04-03 11:14:26 -0600

    didn’t need the chars

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,5 @@
 class Hamming
   def self.compute(strand_A, strand_B)
-    strand_A.chars.length.times.count { |i| strand_A[i] != strand_B[i] }
+    strand_A.length.times.count { |i| strand_A[i] != strand_B[i] }
   end
 end
