1850e0d1be42810c90c68c62ef07ad00b9ebc73a
Travis Yoder <trayoda@gmail.com>
2015-04-09 15:29:10 -0600

    used the built in math.min to find the shorter strand

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -2,10 +2,9 @@ local Hamming = {}
 
 function Hamming.compute(strand_A, strand_B)
   difference = 0
-  shorter_strand = #strand_A < #strand_B and #strand_A or #strand_B
 
-  for i = 1, shorter_strand do
-    if strand_A:sub(i,i) ~= strand_B:sub(i,i) then
+  for i = 1, math.min(#strand_A, #strand_B) do
+    if strand_A:sub(i, i) ~= strand_B:sub(i, i) then
       difference = difference + 1
     end
   end
