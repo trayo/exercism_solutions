e6df548f6ca6b695bfaf1713083ef19a737363b5
Travis Yoder <trayoda@gmail.com>
2015-04-16 15:45:34 -0600

    completed ETL for java

---
+++ java/etl/src/main/java/Etl.java
@@ -0,0 +1,26 @@
+import com.google.common.collect.ImmutableMap;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+
+
+
+public class Etl {
+	public Map<String, Integer> transform(Map<Integer, List<String>> old){
+
+		Map<String, Integer> sorted = new TreeMap<String, Integer>();
+
+		for (Entry<Integer, List<String>> entry : old.entrySet()) {
+			for (int i = 0; i < entry.getValue().size(); i++) {
+				sorted.put(entry.getValue().get(i).toLowerCase(), entry.getKey());
+			}
+		}
+		
+		sorted = new ImmutableMap.Builder<String, Integer>().putAll(sorted).build();
+		
+		return sorted;
+	}
+}
