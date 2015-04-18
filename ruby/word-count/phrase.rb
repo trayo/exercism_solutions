af7dcf45b98be94b95cda6a6ffd9cc1e8964afb0
Travis Yoder <trayoda@gmail.com>
2015-04-18 09:13:20 -0600

    changed to hash.new(0) so i don't have to memoize 0

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -4,8 +4,7 @@ class Phrase
   end
 
   def word_count
-    @phrase.each_with_object({}) do |word, result|
-      result[word] ||= 0
+    @phrase.each_with_object(Hash.new(0)) do |word, result|
       result[word] += 1
     end
   end
