b99e1fcfcc0738981617aefcc0af1af93872cd3b
Travis Yoder <trayoda@gmail.com>
2015-04-19 11:49:10 -0600

    added a parse private method

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,11 +1,17 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase.downcase.gsub(/[^\w']/, " ").split
+    @phrase = parse(phrase)
   end
 
   def word_count
     @phrase.each_with_object(Hash.new(0)) do |word, result|
       result[word] += 1
     end
+  end
+
+  private
+
+  def parse(phrase)
+    phrase.downcase.gsub(/[^\w']/, " ").split
   end
 end
