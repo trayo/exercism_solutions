81d696bc67509cf1c3b1eeada5cb3a24332aade5
Travis Yoder <trayoda@gmail.com>
2015-04-24 12:27:23 -0600

    clock in progress and pulled gigasecond

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,9 +2,29 @@ package clock
 
 import "fmt"
 
+type Clock struct {
+  hour, minutes int
+}
+
 const TestVersion = 2
 
-func Time(hour, minutes int) {
-  fmt.Println(hour)
-  fmt.Println(minutes)
+// func (clock Clock) Add(minutes int) Clock {
+
+//   return Time(minutes)
+// }
+
+func (clock Clock) String() string {
+  "hours: clock"
+}
+
+func Time(hours, minutes int) Clock {
+  hoursToMinutes := hours * 60
+  total := minutes + hoursToMinutes
+
+  h := total / 60
+  m := total % 60
+
+  return Clock(hour: h, minutes: m)
+  // fmt.Println(hour)
+  // fmt.Println(minutes)
 }
