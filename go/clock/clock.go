b3d298d8c3be8f0c45fc606ff6c5453ecc3a90f6
Travis Yoder <trayoda@gmail.com>
2015-04-26 11:29:04 -0600

    made the adjust functions properties on the Clock type

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -9,43 +9,40 @@ type Clock struct {
 const TestVersion = 2
 
 func Time(hours, minutes int) Clock {
-  hours, minutes = adjustMinutesAndHours(hours, minutes)
-
-  return Clock{hours, minutes}
+  return Clock{hours, minutes}.adjustMinutesAndHours()
 }
 
-func (c Clock) Add(m int) Clock {
-  c.minutes += m
-  c.hours, c.minutes = adjustMinutesAndHours(c.hours, c.minutes)
-  return c
+func (c Clock) Add(minutes int) Clock {
+  c.minutes += minutes
+  return c.adjustMinutesAndHours()
 }
 
 func (c Clock) String() string {
   return fmt.Sprintf("%0.2d:%0.2d", c.hours, c.minutes)
 }
 
-func adjustMinutesAndHours(hours, minutes int) (int, int) {
-  for minutes > 59 || minutes < 0 {
-    if minutes < 0 {
-      hours -= 1
-      minutes += 60
+func (c Clock) adjustMinutesAndHours() Clock {
+  for c.minutes > 59 || c.minutes < 0 {
+    if c.minutes < 0 {
+      c.hours -= 1
+      c.minutes += 60
     } else {
-      hours += 1
-      minutes -= 60
+      c.hours += 1
+      c.minutes -= 60
     }
   }
-  hours = adjustHours(hours)
-  return hours, minutes
+  c = c.adjustHours()
+  return c
 }
 
-func adjustHours(hours int) int {
-  for hours > 23 || hours < 0 {
-    if hours < 0 {
-      hours += 24
+func (c Clock) adjustHours() Clock {
+  for c.hours > 23 || c.hours < 0 {
+    if c.hours < 0 {
+      c.hours += 24
     } else {
-      hours -= 24
+      c.hours -= 24
     }
   }
-  return hours
+  return c
 }
 
