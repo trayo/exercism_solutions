37505c3be1670f95ba5be31016e96a06ff448914
Travis Yoder <trayoda@gmail.com>
2015-04-26 11:03:19 -0600

    all tests working

--- go/clock/clock_test.go
+++ go/clock/clock_test.go
@@ -1,7 +1,7 @@
 package clock
 
 import (
-	// "reflect"
+	"reflect"
 	"testing"
 )
 
@@ -40,30 +40,30 @@ func TestCreateClock(t *testing.T) {
 	t.Log(len(timeTests), "test cases")
 }
 
-// func TestAddMinutes(t *testing.T) {
-// 	for _, a := range addTests {
-// 		if got := Time(a.h, a.m).Add(a.a); got.String() != a.want {
-// 			t.Fatalf("Time(%d, %d).Add(%d) = %q, want %q",
-// 				a.h, a.m, a.a, got, a.want)
-// 		}
-// 	}
-// 	t.Log(len(addTests), "test cases")
-// }
+func TestAddMinutes(t *testing.T) {
+	for _, a := range addTests {
+		if got := Time(a.h, a.m).Add(a.a); got.String() != a.want {
+			t.Fatalf("Time(%d, %d).Add(%d) = %q, want %q",
+				a.h, a.m, a.a, got, a.want)
+		}
+	}
+	t.Log(len(addTests), "test cases")
+}
 
-// func TestCompareClocks(t *testing.T) {
-// 	for _, e := range eqTests {
-// 		clock1 := Time(e.c1.h, e.c1.m)
-// 		clock2 := Time(e.c2.h, e.c2.m)
-// 		got := clock1 == clock2
-// 		if got != e.want {
-// 			t.Log("Clock1:", clock1)
-// 			t.Log("Clock2:", clock2)
-// 			t.Logf("Clock1 == Clock2 is %t, want %t", got, e.want)
-// 			if reflect.DeepEqual(clock1, clock2) {
-// 				t.Log("(Hint: see comments in clock_test.go.)")
-// 			}
-// 			t.FailNow()
-// 		}
-// 	}
-// 	t.Log(len(eqTests), "test cases")
-// }
+func TestCompareClocks(t *testing.T) {
+	for _, e := range eqTests {
+		clock1 := Time(e.c1.h, e.c1.m)
+		clock2 := Time(e.c2.h, e.c2.m)
+		got := clock1 == clock2
+		if got != e.want {
+			t.Log("Clock1:", clock1)
+			t.Log("Clock2:", clock2)
+			t.Logf("Clock1 == Clock2 is %t, want %t", got, e.want)
+			if reflect.DeepEqual(clock1, clock2) {
+				t.Log("(Hint: see comments in clock_test.go.)")
+			}
+			t.FailNow()
+		}
+	}
+	t.Log(len(eqTests), "test cases")
+}
