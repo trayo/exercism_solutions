14447dab3b6fcae2f82612563f75144ed7430cb6
Travis Yoder <trayoda@gmail.com>
2015-04-26 10:15:25 -0600

    extracted some of the adjusting minutes and hours functionality

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -3,41 +3,47 @@ package clock
 import "fmt"
 
 type Clock struct {
-	hours, minutes int
+  hours, minutes int
 }
 
 const TestVersion = 2
+
+func Time(hours, minutes int) Clock {
+  hours, minutes = adjustMinutesAndHours(hours, minutes)
+
+  return Clock{hours, minutes}
+}
 
 // func (clock Clock) Add(minutes int) Clock {
 //   return Time(minutes)
 // }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%0.2d:%0.2d", c.hours, c.minutes)
+  return fmt.Sprintf("%0.2d:%0.2d", c.hours, c.minutes)
 }
 
-func AdjustHours(h int) int {
-	for h > 23 || h < 0 {
-		if h < 0 {
-			h += 24
-		} else {
-			h -= 24
-		}
-	}
-	return h
+func adjustMinutesAndHours(hours, minutes int) (int, int) {
+  for minutes > 59 || minutes < 0 {
+    if minutes < 0 {
+      hours -= 1
+      minutes += 60
+    } else {
+      hours += 1
+      minutes -= 60
+    }
+  }
+  hours = adjustHours(hours)
+  return hours, minutes
 }
 
-func Time(hours, minutes int) Clock {
-	for minutes > 59 || minutes < 0 {
-		if minutes < 0 {
-			hours -= 1
-			minutes += 60
-		} else {
-			hours += 1
-			minutes -= 60
-		}
-	}
-
-	hours = AdjustHours(hours)
-	return Clock{hours, minutes}
+func adjustHours(hours int) int {
+  for hours > 23 || hours < 0 {
+    if hours < 0 {
+      hours += 24
+    } else {
+      hours -= 24
+    }
+  }
+  return hours
 }
+
