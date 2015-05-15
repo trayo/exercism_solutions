7459c5917c24b370d7de0940aa008d2bfce332d7
Travis Yoder <trayoda@gmail.com>
2015-05-15 09:34:46 -0600

    update to hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,6 @@
 class Hamming
   def self.compute(strand_A, strand_B)
+    raise ArgumentError if strand_A.length != strand_B.length
     strand_A.length.times.count { |i| strand_A[i] != strand_B[i] }
   end
 end
