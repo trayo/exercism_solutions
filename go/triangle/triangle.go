506b2d1d34e4ccbf7cc6a5feb4832d0ff34df213
Travis Yoder <trayoda@gmail.com>
2015-05-23 11:44:19 -0600

    making progress on go triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,12 +1,32 @@
 package triangle
 
+import (
+  "fmt"
+  "sort"
+)
+
 type Kind string
 
-var Equ Kind
-var Iso Kind
-var Sca Kind
-var NaT Kind
+var Equ = Kind("Equ")
+var Iso = Kind("Iso")
+var Sca = Kind("Sca")
+var NaT = Kind("NaT")
 
-func KindFromSides(a, b, c float64) (result Kind) {
-	return
+func KindFromSides(a, b, c float64) Kind {
+  fs, ss := findShortSides(a, b, c)
+
+
+  fmt.Sprintln("fs: %s, ss: %s", fs, ss)
+
+
+  if a <= 0 || b <= 0 || c <= 0 {
+    return NaT
+  } else if a==b && b==c && c==a {
+    return Equ
+  } else if a==b || b==c || c==a {
+    return Iso
+  } else {
+    return Sca
+  }
 }
+
