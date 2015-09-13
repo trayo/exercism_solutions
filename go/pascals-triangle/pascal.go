2467509ee2a39a8d74e35ac45df50b998b0bcbd9
Travis Yoder <trayoda@gmail.com>
2015-09-13 13:22:35 -0700

    ran go fmt

--- go/pascals-triangle/pascal.go
+++ go/pascals-triangle/pascal.go
@@ -1,24 +1,23 @@
 package pascal
 
 func Triangle(numberOfRows int) [][]int {
-  triangle := [][]int{[]int{1}}
+	triangle := [][]int{[]int{1}}
 
-  if numberOfRows <= 1 {
-    return triangle
-  }
+	if numberOfRows <= 1 {
+		return triangle
+	}
 
-  for previousRow := 0; previousRow < numberOfRows-1; previousRow++ {
-    rowData := triangle[previousRow]
-    newRow := []int{1}
+	for previousRow := 0; previousRow < numberOfRows-1; previousRow++ {
+		rowData := triangle[previousRow]
+		newRow := []int{1}
 
-    for i := 1; i < len(rowData); i++ {
-      newRow = append(newRow, rowData[i-1] + rowData[i])
-    }
+		for i := 1; i < len(rowData); i++ {
+			newRow = append(newRow, rowData[i-1]+rowData[i])
+		}
 
-    newRow = append(newRow, 1)
-    triangle = append(triangle, newRow)
-  }
+		newRow = append(newRow, 1)
+		triangle = append(triangle, newRow)
+	}
 
-  return triangle
+	return triangle
 }
-
