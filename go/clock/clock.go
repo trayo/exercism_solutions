d1bfa7b2275b7059f986988ee18229b3e47e25c9
Travis Yoder <trayoda@gmail.com>
2016-02-05 20:26:04 -0700

    fix some formatting in clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -5,44 +5,47 @@ import "fmt"
 const TestVersion = 2
 
 type Clock struct {
-  hours, minutes int
+	hours, minutes int
 }
 
 func Time(hours, minutes int) Clock {
-  return adjustMinutesAndHours(Clock{hours, minutes})
+	c := Clock{hours, minutes}
+	return c.adjustMinutesAndHours()
 }
 
 func (c Clock) Add(minutes int) Clock {
-  c.minutes += minutes
-  return adjustMinutesAndHours(c)
+	c.minutes += minutes
+	return c.adjustMinutesAndHours()
 }
 
 func (c Clock) String() string {
-  return fmt.Sprintf("%0.2d:%0.2d", c.hours, c.minutes)
+	return fmt.Sprintf("%0.2d:%0.2d", c.hours, c.minutes)
 }
 
-func adjustMinutesAndHours(c Clock) Clock {
-  for c.minutes > 59 || c.minutes < 0 {
-    if c.minutes < 0 {
-      c.hours -= 1
-      c.minutes += 60
-    } else {
-      c.hours += 1
-      c.minutes -= 60
-    }
-  }
-
-  return adjustHours(c)
+func (c Clock) adjustMinutesAndHours() Clock {
+	c.adjustMinutes()
+	c.adjustHours()
+	return c
 }
 
-func adjustHours(c Clock) Clock {
-  for c.hours > 23 || c.hours < 0 {
-    if c.hours < 0 {
-      c.hours += 24
-    } else {
-      c.hours -= 24
-    }
-  }
-  return c
+func (c *Clock) adjustMinutes() {
+	for c.minutes > 59 || c.minutes < 0 {
+		if c.minutes < 0 {
+			c.hours -= 1
+			c.minutes += 60
+		} else {
+			c.hours += 1
+			c.minutes -= 60
+		}
+	}
 }
 
+func (c *Clock) adjustHours() {
+	for c.hours > 23 || c.hours < 0 {
+		if c.hours < 0 {
+			c.hours += 24
+		} else {
+			c.hours -= 24
+		}
+	}
+}
